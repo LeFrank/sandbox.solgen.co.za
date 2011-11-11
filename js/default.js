f2d4c28aed1a64ada785873f518113249dc2d0e0
Francois Campbell <campbellfd@gmail.com>
2011-11-11 08:03:16 +0200

    menu stuff

--- js/default.js
+++ js/default.js
@@ -1,8 +1,20 @@
 
 $(document).ready(function() {
     // put all your jQuery goodness in here.
-    $(".nav a").click(
+    $(".main_nav").mouseover(
         function (){
-            alert("get next level");
-        });
+            $("#"+this.id+" #main_nav_drop").toggle().attr("inline", "none");
+    });
+    $(".main_nav").mouseout(
+        function (){
+            $("#"+this.id+" #main_nav_drop").toggle().attr("inline", "none");
+    });
+    $(".main_nav").click(
+        function (){
+            //ajax function to get content for now just putting something in to triger frameset activation.
+            $("#"+this.id+" #sub_nav").toggle().attr("inline", "none").html("<a href='/projects/css/index.php' target='content_frame' >CSS Project home page.</a>");
+    });
 });
+function show_drop_down(ele){
+}
+
--- nav.php
+++ nav.php
@@ -1,3 +1,17 @@
+<!doctype html>
+<html>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+        <title>Solgen Sandbox  :) </title>
+        <link rel="stylesheet" type="text/css" media="all" href="/css/style.css" />
+        <link href="http://fonts.googleapis.com/css?family=Oswald:400,700" rel="stylesheet" type="text/css"/>
+        <script src="/js/third_party/modernizr-latest.js"></script>
+        <script src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
+        <script src="/js/default.js"></script>
+    </head>
+        <body class="body" style="overflow:hidden;">
+        <div class="body_content">
+            <div class="content">
 <?php
 function build_tree($dir="projects"){
     $excpts_arr = array(".",".." , "nbproject" , ".git" , ".svn");
@@ -19,6 +33,9 @@ function build_tree($dir="projects"){
 
 $nav = build_tree();
 foreach($nav as $dir_name=>$dir_path) {
-    echo "<a href='".str_replace(DOC_ROOT , "" ,$dir_path)."/index.php'>".$dir_name."</a>";
+    echo "<div id='".$dir_name."'  class='main_nav'  target ='showframe' >".$dir_name."<span id='main_nav_drop'><img src='/images/bg_images/down.png' /></span><div id='sub_nav'>&nbsp;<br/></div></div><br/>";
 }
-?>
+?></div>
+</div>
+</body>
+</html>
