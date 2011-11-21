74f1232aaafa9793e333fbdc48877d153e1ad893
Francois Campbell <campbellfd@gmail.com>
2011-11-22 00:26:56 +0200

    content stuff

--- projects/fonts/index.php
+++ projects/fonts/index.php
@@ -7,4 +7,5 @@
 ?>
 <ol>
     <li>Mr Moustache - <a href='http://new.myfonts.com/fonts/facetype/mr-moustache/'  target="_blank">-=-</a></li>
+    <li>Fico - <a href='http://fico.lensco.be/'  target="_blank">Fico is a web font but with vector icons </a></li>
 </ol>
