2cd5957d7f8a51f389930a4fc77f4976a3424f7d
Francois Campbell <francois@cpt.whitewallweb.com>
2011-11-22 16:51:24 +0200

    html5 utf-8 meta tag

---
+++ projects/html/index.php
@@ -0,0 +1,8 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+Special Chars go away - <code><?= htmlentities('<meta http-equiv="content-type" content="text/html; charset=utf-8">')?></code>
