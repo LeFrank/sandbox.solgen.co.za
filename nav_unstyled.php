321e40988cb4d9d7a7524443afc8328f721c7474
Francois Campbell <campbellfd@gmail.com>
2011-11-24 01:14:14 +0200

    html hover/pull down nav ala code igniter help section

--- nav_unstyled.php
+++ nav_unstyled.php
@@ -2,7 +2,7 @@
 include 'nav_xml.php';
 
 $navigation = new SimpleXMLElement($navigation_xml);
-$str = "<div style='border:solid 2px red;'>";
+$str = "<div style='text-align:left;'>";
 foreach($navigation->nav->projects->project_category as $item) {
     $str2 = "";
     if(isset($item->project)) {
@@ -12,7 +12,7 @@ foreach($navigation->nav->projects->project_category as $item) {
         }
         $str2 .="</ul>";
     }
-    $str .= "<div style='width:200px;float:left;position:relative;border:solid 1px #ccc;'>".trim($item->name)."<br/>".$str2."</div>";
+    $str .= "<div style='width:200px;float:left;position:relative;border:solid 2px #ccc;'>".trim($item->name)."<br/>".$str2."</div>";
 }
 $str.'</div>';
 echo $str;
