e0440009ccd6f47ea774737b1a83373ff5432c50
Francois Campbell <campbellfd@gmail.com>
2011-12-20 00:47:31 +0200

    more css menu thinging

--- nav_unstyled.php
+++ nav_unstyled.php
@@ -2,17 +2,17 @@
 include 'nav_xml.php';
 
 $navigation = new SimpleXMLElement($navigation_xml);
-$str = "<div style='text-align:left;' id='nav_container'>";
+$str = "<div id='nav_container'>";
 foreach($navigation->nav->projects->project_category as $item) {
     $str2 = "";
     if(isset($item->project)) {
-        $str2 .="<ul>";
+//        $str2 .="<ul>";
         foreach($item->project as $proj) {
-            $str2 .= "<li><a href='".$proj->path."'>".$proj->name."</a></li>";
+            $str2 .= "<a href='".$proj->path."'>".$proj->name."</a><br/>";
         }
-        $str2 .="</ul>";
+//        $str2 .="</ul>";
     }
-    $str .= "<div style='width:200px;float:left;border:1px solid grey;margin:5px 10px;' id='nav_cat_item'><a href='".$item->path."'>".trim($item->name)."</a><br/>".$str2."</div>";
+    $str .= "<div  id='nav_cat_item' class='nav_cat_item'><a href='".$item->path."'>".trim($item->name)."</a><br/>".$str2."</div>";
 }
 $str.='</div>';
 echo $str;
